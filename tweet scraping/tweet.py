582405edbd593db9763582f016bcc02f31ce74c9
KizMan-23 <kizitougwu777@gmail.com>
2024-11-12 11:11:26 +0100

    walmart scraping starts

--- tweet scraping/tweet.py
+++ tweet scraping/tweet.py
@@ -6,8 +6,8 @@ from configparser import ConfigParser
 from random import randint
 
 
-MINIMUM_TWEETS = 20
-QUERY = 'bitcoin'
+MINIMUM_TWEETS = 30  #increase the number of min tweets
+QUERY = 'bitcoin' #use X Advanced Search for complex or more specific queries
 
 ## login credentials
 config = ConfigParser()
@@ -15,6 +15,11 @@ config.read('config.ini')
 username = config['X']['username']
 password = config['X']['password']
 email = config['X']['email']
+
+# create csv file
+with open("bitcoin_tweets.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow('Tweet_count', 'Username', 'Text', 'Created At', 'Retweets', 'Likes')
 
 # authenticate to X.com
 #1) Use login credentials 2) use cookies
@@ -24,12 +29,40 @@ client = Client(language='en-US')
 
 client.load_cookies('cookies.json')
 
-#get tweets
-tweets = client.search_tweet(QUERY, product='Latest')
+def get_tweets(tweets): 
+    if tweets is None:
+        print(f'{datetime.now()} - Getting tweets..')
+        #get tweets
+        tweets = client.search_tweet(QUERY, product='Latest')
+    else:
+        wait_time = randint(5,10)
+        print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds')
+        time.sleep(wait_time)
+        tweets = tweets.next()
+    return tweets
 
-print(tweets)
+tweet_count = 0
+tweets = None
+
+while tweet_count < MINIMUM_TWEETS:
+    try:
+        tweets = get_tweets(tweets)
+    except TooManyRequests as e:   #To allow for time rest when rate limit is hit
+        rate_limit_reset = datetime.fromtimestamp(e.rate_limit_reset)
+        print(f'{datetime.now()} - Rate limit reached. Waiting until {rate_limit_reset}')
+        wait_time = rate_limit_reset - datetime.now()
+        time.sleep(wait_time.total_seconds())
+
+    if not tweets:
+        print(f'{datetime.now()} - No more tweets')
+        break
+
+    for tweet in tweets:
+        tweet_count += 1
+        tweet_data = [tweet_count, tweet.user.name, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favourite_count]
+        with open('bitcoin_tweets.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(tweet_data)
 
 
-for tweet in tweets:
-    print(vars(tweet))
-    break
+print(f'{datetime.now()} - Done got {tweet_counts} tweets found')
