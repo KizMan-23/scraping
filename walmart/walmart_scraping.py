c49903026fd69785e60efb6c6e658fda7956e089
KizMan-23 <kizitougwu777@gmail.com>
2024-11-14 12:56:27 +0100

    improvements

--- walmart/walmart_scraping.py
+++ walmart/walmart_scraping.py
@@ -24,14 +24,31 @@ with open("page_resp.html") as f:
 
 soup = BeautifulSoup(response.text, "html.parser")
 main_page = soup.find('div', id= "__next")
+image_container = main_page.find_all('img')
+
+
 product_description = main_page.find('h1', class_ = 'lh-copy dark-gray mv1 f4 mh0-l mh3 b').text
 #Lesson: "find_all" returns a list of contents. Better to use "find"
-
 brand_name = main_page.find("div", class_= "mt0 mh0-l mh3").text
+
+product_img_url = []
+for img in image_container:
+    format_description = product_description.replace(" ", "-").lower()
+    img_url = img.get("src")
+
+    if "https://i5.walmartimages.com/seo/" in img_url:
+        if format_description in img_url.lower():
+            all_link = img_url
+            product_img_url.append(all_link)
+    
+
 discount_price = main_page.find("span", class_ = "inline-flex flex-column").text.split()[1]
 original_price = main_page.find("span", class_ = "mr2 f6 gray strike").text
-rating = main_page.find("span", class_ = "w_iUH7").text
+rating = main_page.find("span", class_ = "f7 ph1")
+avg_rating = rating.text
+number_of_reviews = rating.find_parent("div", class_ = "flex items-center mr2").find("a", href=True).text
 color = main_page.find("div", class_ = "mid-gray mb2").text.split()[1]
+
 
 option_list = main_page.find("div", class_ = "flex flex-wrap nl1 nr1")
 option_a = option_list.find("div", class_ = "flex flex-column bg-white")
@@ -42,7 +59,7 @@ while option_a:
     option_a = option_a.find_next_sibling("div", class_ = "flex flex-column bg-white")
 
     
-print(available_colors)
+#print(image_container)
 
 
 #enter each section
